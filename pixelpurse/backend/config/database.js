1fd92f0a8627c7130b2cfb7a9f1660faae90535a
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-30 00:19:00 +0530

    connected to database

--- pixelpurse/backend/config/database.js
+++ pixelpurse/backend/config/database.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/pixelpurse", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-}).then(()=> {
-    console.log("Connected to MongoDB");
-});
+const connectDatabase = () => {
+    mongoose.connect(process.env.DB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }).then((data)=> {
+          console.log(`Connected to MongoDB with server ${data.connection.host}`);
+      });
+};
+
+module.exports = connectDatabase;
--- pixelpurse/backend/server.js
+++ pixelpurse/backend/server.js
@@ -1,9 +1,14 @@
 const app = require('./app');
 const dotenv = require('dotenv');
+const connectDatabase = require('./config/database');
 
 //config
 
 dotenv.config({ path: "backend/config/config.env" });
+
+//connect to database
+
+connectDatabase();
 
 app.listen(process.env.PORT, () => {
   console.log(`Listening on port http://localhost:${process.env.PORT}`);
