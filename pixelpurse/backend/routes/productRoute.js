e7a3f0858f2df9c44f7aed0f5a8a049cfd17e2b7
shashank-mishra14 <shashank.mishra.dev14@gmail.com>
2023-12-30 01:08:52 +0530

    deleting a product

--- pixelpurse/backend/routes/productRoute.js
+++ pixelpurse/backend/routes/productRoute.js
@@ -6,5 +6,5 @@ const router = express.Router();
 
 router.route('/products').get(getAllProducts);
 router.route('/products/new').post(createProduct);
-router.route('/products/:id').put(updateProduct);
+router.route('/products/:id').put(updateProduct).delete(deleteProduct);
 module.exports = router;
